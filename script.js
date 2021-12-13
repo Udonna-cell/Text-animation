b519a813b462e5333e838141eccfddcec9325e10
Stanley-Orabueze <67174967+Udonna-cell@users.noreply.github.com>
2021-12-13 19:28:58 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let text = document.querySelector('div:first-child');
+let name = document.querySelector('h3');
+let title = document.querySelector('p');
+
+window.addEventListener('load',()=>{
+  text.style.width = '50%';
+  name.style.right = '0px';
+  title.style.bottom = '0px';
+});
